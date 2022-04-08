d6bfc161b7bc70aa9f238527a4423bf8c3aea458
davide <davidebotti92@gmail.com>
2022-04-08 10:43:50 +0200

    react components basics 1.35.17

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Greeting() {
+  return (
+    <div>
+      <Person/>
+      <Message/>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Person = () => <h2>Davide Botti</h2>
+const Message = () => {
+  return (
+    <p>this is a message</p>
+  )
+}
+
+ReactDom.render(<Greeting/>, document.getElementById('root'));
+
+// REACT COMPONENTS RULES
+// stateless functional component
+// ALWAYS return JSX
+// return single element
+// className instead of class
+// use parenthesis after the return
+
+const Greeting3 = () => {
+  return (
+    <div className=''>
+      <h3>hello people</h3>
+      <ul>
+        <li>
+          <a href='#'>hello world</a>
+        </li>
+        <input type='text' name='' id=''></input>
+      </ul>
+      <div></div>
+    </div>
+  )
+}
+
+function Greeting5() {
+  return <h4>this is Davide and this is my first component</h4>;
+}
+
+const Greeting2 = () => {
+  return React.createElement('h1', {}, 'hello world')
+}
+
+const Greeting4 = () => {
+  return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'))
+}
