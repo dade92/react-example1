86d27ceafb22e015d312aa61b8927de99bc1f08f
davide <davidebotti92@gmail.com>
2022-04-08 11:03:03 +0200

    React components 1.44

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+function BookList() {
+  return (
+  <section> 
+    <Book/>
+   </section>
+  );
+}
+
+const Book = () => {
+  return (
+  <article> 
+    <Image/> 
+    <Title/>  
+    <Author/>
+  </article>
+  )
+}
+const Image = () => <img src='https://images-na.ssl-images-amazon.com/
+images/I/71PmVNpXSUL._UX300__PJku-headphones-v1,TopRight,0,-50_AC_UL381_SR381,381_.jpg' alt=''/>
+
+const Title = () => <h1>My Evil Mother: A Short Story</h1>
+
+const Author = () => <h4>Margaret Atwood</h4>
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
+
+// REACT COMPONENTS RULES
+// stateless functional component
+// ALWAYS return JSX
+// return single element
+// className instead of class
+// use parenthesis after the return
+
 function Greeting() {
   return (
     <div>
@@ -16,15 +50,6 @@ const Message = () => {
     <p>this is a message</p>
   )
 }
-
-ReactDom.render(<Greeting/>, document.getElementById('root'));
-
-// REACT COMPONENTS RULES
-// stateless functional component
-// ALWAYS return JSX
-// return single element
-// className instead of class
-// use parenthesis after the return
 
 const Greeting3 = () => {
   return (
