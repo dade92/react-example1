4da0d2ed2718ebb737f2608cec30ddee77c5b4f6
davide <davidebotti92@gmail.com>
2022-04-10 19:26:55 +0200

    add events

--- src/Books.jsx
+++ src/Books.jsx
@@ -1,13 +1,23 @@
 export const Book = (props) => {
     const {img, title, author, rating} = props.book;
+    const complex = (author) => {
+      console.log(author)
+    }
+    const click = (e) => {
+      console.log(e)
+      console.log(e.target)
+    }
     return (
     <article className='book'>
       <img src={img} alt=''/>
-      <h1>{title}</h1>
+      <h1 onClick={()=>console.log(title)}>{title}</h1>
       <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>
         {author.toUpperCase()}
       </h4>
       <h4>Rating: {rating}</h4>
+      <button type='button' onClick={click}>
+        Buy now
+      </button>
     </article>
     )
   }
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { Book } from './Books'
 import './index.css';
 const books = [
   {
+    id:1,
     img : 'https://images-na.ssl-images-amazon.com/images/I/71PmVNpXSUL._UX300__PJku-headphones-v1,TopRight,0,-50_AC_UL381_SR381,381_.jpg',
     title: 'My evil mother',
     author: 'Margaret Atwood',
     rating: 4
   },
   {
+    id:2,
     img : 'https://m.media-amazon.com/images/I/41ya6frLgGL.jpg',
     title: 'Strangers We Know',
     author: 'Elle Marr',
@@ -25,7 +27,7 @@ function BookList() {
     <section className='booklist'> 
       {books.map((book) => {
             return (
-              <Book book={book}></Book>
+              <Book key={book.id} book={book}></Book>
             )
       })
       }
