a605375b897820c9147e36abf5cc8163a7191ea6
dbotti <davide.botti@lastminute.com>
2022-04-15 09:28:47 +0200

    New on click

--- src/Book.jsx
+++ src/Book.jsx
@@ -3,27 +3,26 @@ import React from 'react'
 const Book = (props) => {
     const {img, title, author, rating} = props.book;
     const complex = (author) => {
-      console.log(author)
+        console.log(author)
     }
-    const click = (e) => {
-      console.log(e)
-      console.log(e.target)
+    const click = (title, author) => {
+        console.log(author + ":" + title)
     }
     return (
-    <article className='book' onMouseOver={() => {
-      console.log(title)
-    }}>
-      <img src={img} alt=''/>
-      <h1 onClick={()=>console.log(title)}>{title}</h1>
-      <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>
-        {author.toUpperCase()}
-      </h4>
-      <h4>Rating: {rating}</h4>
-      <button type='button' onClick={click}>
-        Buy now
-      </button>
-    </article>
+        <article className='book' onMouseOver={() => {
+            console.log(title)
+        }}>
+            <img src={img} alt=''/>
+            <h1 onClick={() => console.log(title)}>{title}</h1>
+            <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>
+                {author.toUpperCase()}
+            </h4>
+            <h4>Rating: {rating}</h4>
+            <button type='button' onClick={()=>click(title, author)}>
+                Buy now
+            </button>
+        </article>
     )
-  }
+}
 
-  export default Book;
+export default Book;
